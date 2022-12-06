565c48f4b013d31abeed58fadc648462d2475922
cartige <cartiaux.gerald@gmail.com>
2022-12-06 16:40:05 +0100

    first pages

--- frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <header className="App-header">
-      <h1>Hello Wilders</h1>
-    </header>
-  );
-}
---
+++ frontend/src/pages/Search/Search.jsx
@@ -0,0 +1,11 @@
+import "./search.scss";
+import NavBar from "../../components/NavBar/NavBar";
+
+export default function Search() {
+  return (
+    <div className="search">
+      <h1>Search</h1>
+      <NavBar />
+    </div>
+  );
+}
