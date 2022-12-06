a50af019dcb561c0a982d24b683cd97d261f7bad
cartige <cartiaux.gerald@gmail.com>
2022-12-06 16:40:05 +0100

    first pages

---
+++ frontend/src/pages/Profil/Profil.jsx
@@ -0,0 +1,11 @@
+import "./profil.scss";
+import NavBar from "../../components/NavBar/NavBar";
+
+export default function Profil() {
+  return (
+    <div className="profil">
+      <h1>Profil</h1>
+      <NavBar />
+    </div>
+  );
+}
