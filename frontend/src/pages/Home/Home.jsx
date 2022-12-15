ec11dc646f2745260e47f2e2f39e6f6ca8e943bb
Fostrankill <romain.pourchaux@gmail.com>
2022-12-15 12:06:37 +0100

    YoutubeHomePage

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,32 @@
 import "./home.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Mousewheel, Keyboard, FreeMode } from "swiper";
+import YouTube from "react-youtube";
 import NavBar from "../../components/NavBar/NavBar";
 /* eslint-disable import/no-unresolved */
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+
 /* eslint-enable import/no-unresolved */
 
 export default function Home() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/videos")
+      .then(({ data }) => {
+        setVideos(data);
+      })
+      .catch((err) => {
+        console.warn("------------", err);
+      });
+  }, []);
+
   return (
     <header className="home">
       <Swiper
@@ -19,12 +37,18 @@ export default function Home() {
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
         className="my-swiper"
       >
-        <SwiperSlide>
-          <img
-            src="https://images.affiches-et-posters.com//albums/3/4661/4661-poster-film-drive.jpg"
-            alt=""
-          />
-        </SwiperSlide>
+        {videos.map((video) => {
+          return (
+            <SwiperSlide>
+              <YouTube videoId={video.url} />
+            </SwiperSlide>
+          );
+        })}
+
+        {/* <SwiperSlide>
+          <YouTube videoId="DyBax0Xg3NY" />
+        </SwiperSlide> */}
+
         <SwiperSlide>
           <img
             src="https://fr.web.img4.acsta.net/pictures/22/09/23/15/11/2942764.jpg"
@@ -50,7 +74,7 @@ export default function Home() {
           />
         </SwiperSlide>
       </Swiper>
-      <h2>Films</h2>
+      {/* <h2>Films</h2>
       <Swiper
         slidesPerView={2}
         spaceBetween={8}
@@ -145,7 +169,7 @@ export default function Home() {
             alt=""
           />
         </SwiperSlide>
-      </Swiper>
+      </Swiper> */}
       <div className="img-home">
         <img
           src="https://images.affiches-et-posters.com//albums/3/4661/4661-poster-film-drive.jpg"
