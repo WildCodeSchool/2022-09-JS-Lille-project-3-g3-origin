11bb73304e2394c8ee7c7d152cd05084c4887cd4
Asma <asma.touzani7@gmail.com>
2022-12-16 16:36:13 +0100

    adding search page, responsive

--- frontend/src/components/SearchBardetails/DurationSearch.jsx
+++ frontend/src/components/SearchBardetails/DurationSearch.jsx
@@ -32,62 +32,73 @@ export default function durationSearch() {
           <ul className="underScrolling">
             <li>
               <label>
-                Drame <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Drame
               </label>
             </li>
             <li>
               <label>
-                Comédie <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Comédie
               </label>
             </li>
             <li>
               <label>
-                Horreur <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Horreur
               </label>
             </li>
             <li>
               <label>
-                Science fiction <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Science fiction
               </label>
             </li>
             <li>
               <label>
-                Fantastique <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Fantastique
               </label>
             </li>
             <li>
               <label>
-                Aventure <input type="radio" name="categogie" />
+                <input type="radio" name="categogie" />
+                Aventure
               </label>
             </li>
             <li>
               <label>
-                Action <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Action
               </label>
             </li>
             <li>
               <label>
-                Guerre <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Guerre
               </label>
             </li>
             <li>
               <label>
-                Policier <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Policier
               </label>
             </li>
             <li>
               <label>
-                Western <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Western
               </label>
             </li>
             <li>
               <label>
-                Nature <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" /> Nature
               </label>
             </li>
             <li>
               <label>
-                Histoire <input type="radio" className="categogie" />
+                <input type="radio" className="categogie" />
+                Histoire
               </label>
             </li>
           </ul>
@@ -95,35 +106,30 @@ export default function durationSearch() {
       </div>
       <div>
         <h2>Films</h2>
+        <span className="affiche">
+          <img
+            src="https://fr.web.img5.acsta.net/pictures/16/06/16/12/01/072037.jpg"
+            alt=""
+          />
+          <img
+            src="https://www.kideaz.com/wp-content/uploads/2020/04/kideaz-famille-addams-affiche-films-cultes.jpg"
+            alt=""
+          />
 
-        <img
-          className="div1"
-          src="https://fr.web.img5.acsta.net/pictures/16/06/16/12/01/072037.jpg"
-          alt=""
-        />
+          <img
+            src="https://www.francetvinfo.fr/pictures/HfbCNtBbyAJFRQ3e5oICK_wyyi8/fit-in/720x/2018/11/28/php14pT6E.jpg"
+            alt=""
+          />
+          <img
+            src="https://fr.web.img4.acsta.net/pictures/22/01/20/09/55/4582630.jpg"
+            alt=""
+          />
 
-        <img
-          className="div2"
-          src="https://www.kideaz.com/wp-content/uploads/2020/04/kideaz-famille-addams-affiche-films-cultes.jpg"
-          alt=""
-        />
-
-        <img
-          className="div3"
-          src="https://www.francetvinfo.fr/pictures/HfbCNtBbyAJFRQ3e5oICK_wyyi8/fit-in/720x/2018/11/28/php14pT6E.jpg"
-          alt=""
-        />
-        <img
-          className="div4"
-          src="https://fr.web.img4.acsta.net/pictures/22/01/20/09/55/4582630.jpg"
-          alt=""
-        />
-
-        <img
-          className="div5"
-          src="https://lecranpop.com/wp-content/uploads/2020/01/img-affiche-film-bohemian-rhapsody.jpg"
-          alt=""
-        />
+          <img
+            src="https://lecranpop.com/wp-content/uploads/2020/01/img-affiche-film-bohemian-rhapsody.jpg"
+            alt=""
+          />
+        </span>
       </div>
     </>
   );
