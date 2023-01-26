cb8831abcee434a1e96bb64b2f75f6ce8d4ac3fe
CharlyJuteau <Charly.juteau4@gmail.com>
2023-01-26 15:27:50 +0100

    conflict with classname

--- frontend/src/pages/PresentationPage/PresentationPage.jsx
+++ frontend/src/pages/PresentationPage/PresentationPage.jsx
@@ -23,22 +23,26 @@ export default function PresentationPage() {
     <div className="LoginPage">
       <div className="presentationPage">
         <div className="textPresentationPage">
-          <h2>Bienvenue sur Origins Digital ! </h2>
+          <h2 className="titleTextPresentation">
+            Bienvenue sur Origins Digital !{" "}
+          </h2>
 
           <br />
-          <h3>
+          <h3 className="sentenceTextPresentation">
             Origins Digital est une nouvelle plateforme où vous pourrez regarder
             vos programmes favoris !
           </h3>
           <br />
-          <h3>
+          <h3 className="sentenceTextPresentation">
             Dévouvrez ou alors re-découvrez une selection variées de Films,
             Séries et bien plus encore !
           </h3>
           <br />
-          <h2>Vous souhaitez passer en Premium ? </h2>
+          <h2 className="titleTextPresentation">
+            Vous souhaitez passer en Premium ?{" "}
+          </h2>
           <br />
-          <h3>
+          <h3 className="sentenceTextPresentation">
             Souscrivez à notre offre premium et bénéficiez d'un accès illimité à
             notre catalogue !
           </h3>
@@ -65,29 +69,39 @@ export default function PresentationPage() {
       </div>
       <div className="presentationOfPremium">
         <div className="textOfPremium">
-          <h2>Pourquoi souscrire à notre offre premium ?</h2>
+          <h2 className="titleTextPresentation">
+            Pourquoi souscrire à notre offre premium ?
+          </h2>
           <br />
-          <h3>Accès illimité à notre catalogue.</h3>
+          <h3 className="sentenceTextPresentation">
+            Accès illimité à notre catalogue.
+          </h3>
           <br />
-          <h3>Accès à vos favoris.</h3>
+          <h3 className="sentenceTextPresentation">Accès à vos favoris.</h3>
           <br />
-          <h3>Créer un compte personnalisé.</h3>
+          <h3 className="sentenceTextPresentation">
+            Créer un compte personnalisé.
+          </h3>
           <br />
-          <h3>Voici un tableau pour vous aider à faire votre choix &#10132;</h3>
+          <h3 className="sentenceTextPresentation">
+            Voici un tableau pour vous aider à faire votre choix &#10132;
+          </h3>
           <br />
         </div>
         <img className="backgroundSampleHome" src={chart} alt="Background" />
       </div>
       <div className="presentationOfProfile">
         <div className="textOfProfile">
-          <h2>Toujours pas convaincu pour l'offre premium ?</h2>
+          <h2 className="titleTextPresentation">
+            Toujours pas convaincu pour l'offre premium ?
+          </h2>
           <br />
-          <h3>
+          <h3 className="sentenceTextPresentation">
             Profitez de notre offre gratuite et commencez à personnaliser votre
             profil avec vos films et séries !
           </h3>
           <br />
-          <h3>
+          <h3 className="sentenceTextPresentation">
             Avec votre compte personnalisé, vous pourrez sauvegarder vos films
             et séries et les retrouver dans votre propre liste de lecture !
           </h3>
