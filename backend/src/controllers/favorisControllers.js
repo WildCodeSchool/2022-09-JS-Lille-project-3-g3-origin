54c8196371d2d92d8aa4eeb702a4b7b654e379f7
Priscillia <selvapriscillia@yahoo.fr>
2023-01-27 10:06:20 +0100

    delete code use less

--- backend/src/controllers/favorisControllers.js
+++ backend/src/controllers/favorisControllers.js
@@ -1,88 +1,5 @@
 const models = require("../models");
 
-const browse = (req, res) => {
-  models.favoris
-    .findAll()
-    .then(([rows]) => {
-      res.send(rows);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const read = (req, res) => {
-  models.favoris
-    .find(req.params.id)
-    .then(([rows]) => {
-      if (rows[0] == null) {
-        res.sendStatus(404);
-      } else {
-        res.send(rows[0]);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const edit = (req, res, next) => {
-  const favoris = req.body;
-
-  // TODO validations (length, format...)
-
-  favoris.id = parseInt(req.params.id, 10);
-
-  models.favoris
-    .update(favoris)
-    .then(([result]) => {
-      if (result.affectedRows === 0) {
-        next();
-      } else {
-        res.sendStatus(204);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const add = (req, res) => {
-  const favoris = req.body;
-
-  // TODO validations (length, format...)
-
-  models.favoris
-    .insert(favoris)
-    .then(([result]) => {
-      res.location(`/favoris/${result.insertId}`).sendStatus(201);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const destroy = (req, res) => {
-  const { video_id: videoId, user_id: userId } = req.body;
-  models.favoris
-    .deleteFav(videoId, userId)
-    .then(([result]) => {
-      if (result.affectedRows === 0) {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(204);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
 const like = (req, res) => {
   const { video_id: videoId, user_id: userId } = req.body;
   models.favoris
@@ -106,10 +23,5 @@ const like = (req, res) => {
 };
 
 module.exports = {
-  browse,
-  read,
-  edit,
-  add,
-  destroy,
   like,
 };
