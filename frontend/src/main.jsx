cd64be57fdb29cb5b4f46c422aa2049101577aa9
cartige <cartiaux.gerald@gmail.com>
2023-01-30 14:22:29 +0100

    localStorage v1 and handled redirect with react router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { UserInfosContext } from "./contexts/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserInfosContext>
+      <App />
+    </UserInfosContext>
   </React.StrictMode>
 );
